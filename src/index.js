66f1f038cc04d0c0e1f5e8c2af14de14c398a602
renu <renu.vishwakarma@w3villa.com>
2023-07-20 10:46:17 +0530

    Initial commit of social-app

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <div>
+    <h1>My social app</h1>
+    <p>Having following function</p>
+    <ul>
+      <li>Chat</li>
+      <li>Post</li>
+      <li>Like & comments</li>
+    </ul>
+  </div>
+  
+);
+
